20fba90df960809603c64304bed8570b224d70a2
manniig24 <egatica51@gmail.com>
2019-11-21 17:31:04 -0700

    Adding generage card function

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -1,16 +1,36 @@
-
+const gallery = document.getElementById('gallery');
 
 // -----------------------------
 // Fetch Functions
 // -----------------------------
-fetch('https://randomuser.me/api/?results=12')
-    .then(results => results.json())
-    .then(console.log(results))
+fetch('https://randomuser.me/api/?results=12&nat=us')
+    .then(response => response.json())
+    .then(data => console.log(data.picture.medium))
+    // .then(data => generateCard(data))
+    
 
 // -----------------------------
-// 
+// Helper Functions
 // -----------------------------
-
+function generateCard(data) {
+    let user = data.results;
+    user.forEach(function(){
+        const html = `
+        <div class ="card">
+            <div class = "card-img-container">
+                <img class ="card-img" src="${user.picture.medium}" alt ="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap"> ${user.name.first} ${user.name.last}</h3>
+                <p class="card-text">${user.email}</p>
+                <p class="card-text cap">${user.location.city}</p>
+            </div>
+        </div>
+        `;
+    console.log(html);
+        
+    });
+}
 // -----------------------------
 // 
 // -----------------------------
