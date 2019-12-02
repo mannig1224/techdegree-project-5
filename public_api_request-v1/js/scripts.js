55f141806d5c51718e5eac572e1f5982df2d11fd
manniig24 <egatica51@gmail.com>
2019-12-02 02:12:59 -0700

    Finished the generateCard function and formatted according to objectives

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -11,6 +11,12 @@ modalContainer.style.display = 'none';
 // -----------------------------
 // Fetch Functions
 // -----------------------------
+
+/**
+    * Creates the fetch request using fetchAPI
+    * @param {string} url - the url string to call the API
+    * @return {promise} each command inside this function returns a promise. The function itself doesn't return anything
+    */
 function fetchUrl(url){
     fetch(url)
     .then(response => response.json())
@@ -22,6 +28,12 @@ function fetchUrl(url){
 // -----------------------------
 // Helper Functions
 // -----------------------------
+
+/**
+    * Generates the cards based on the API info into html dynamically
+    * @param {array} data - an array of objects containing all the information from the API
+    * @return {users} we are returning the data for the next function that will be called
+    */
 function generateCard(data) {
     let users = data.results;
     users.forEach(function(user){
@@ -42,13 +54,33 @@ function generateCard(data) {
     return users;
 }
 
+
+/**
+    * Generates the modul and formats it with the data array
+    * @param {string} data - an array of objects containing all the information from the API
+    */
 function generateModule(data) {
 const cardList = document.querySelectorAll('div .card');
-console.log(data);
 
-    for(let i = 0; i < cardList.length; i++) {
-
+    for(let i = 0; i < cardList.length; i++) {       
+        
         cardList[i].addEventListener('click', function () {
+
+            // Regex for the birthday - should probably do this in its own function
+            const birthdayDay = data[i].dob.date.substring(8, 10);
+            const birthdayMonth = data[i].dob.date.substring(5, 7);
+            const birthdayYear = data[i].dob.date.substring(0, 4);
+            const birthday = `Birthday: ${birthdayMonth}/${birthdayDay}/${birthdayYear}`;
+
+            // Regex for the Phone - same as above should probably do this on its own function.
+            const phone = data[i].phone;
+            const phoneFormat =  phone.replace(/\D+/g, '')
+            .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+
+            // Format for address - again should make this its own function
+            const address = `${data[i].location.street.number} ${data[i].location.street.name}. ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}`;
+            
+            // creates an html variable that will constanly be called each time a card is clicked
             const html = `
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -58,13 +90,15 @@ console.log(data);
                         <p class="modal-text">${data[i].email}</p>
                         <p class="modal-text cap">${data[i].location.city}</p>
                         <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
+                        <p class="modal-text">${phoneFormat}</p>
+                        <p class="modal-text">${address}</p>
+                        <p class="modal-text">${birthday}</p>
                     </div>
                 </div>
             
             `;
+
+            // Everytime one of the cards gets called in gets replaces with the new html and can get cancelled by the press of a button
             modalContainer.innerHTML = html;
             modalContainer.style.display = '';
             modalContainer.addEventListener('click', (event) => {
@@ -78,5 +112,5 @@ console.log(data);
    
 }
 
-
+// Call the function thats starts the whole program
 fetchUrl(url);
